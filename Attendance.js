104eea6d6ca55dae3cb444dce35f42b664817edf
Erza0838 <muhammaderza884@gmail.com>
2022-11-27 21:33:59 +0700

    commit berhasil

--- Attendance.js
+++ Attendance.js
@@ -15,61 +15,47 @@ const randomNumber = document.getElementById("Attendance_Random_Code")
 // Object untuk menyimpan variabel input 
 let inputData = 
 {
-	NameStudents : studentsName,
-	NumberStudents : studentsNumber,
+	NameStudents : studentsName.value,
+	NumberStudents : studentsNumber.value,
 	RandomNumberProperty : randomNumber,
 	FormContainer : bodyForm,
 	SubmitStudentsData : sendData,	
 	CheckInputData : function CheckStudentsNameInput()
 					{	
-						const formBody = this.FormContainer
+						let disabledButton = inputData.SubmitStudentsData
 
-						let nameAndNumberValue = [this.NameStudents, this.NumberStudents]
+						var actionSubmit = this.FormContainer
 
-						if(Object.values(this).includes(studentsName.value) && Object.values(this).includes(studentsNumber.value) !== null)
+						let randomNumberValue = this.RandomNumberProperty
+
+						if(Object.values(this).includes(studentsName.value) && Object.values(this).includes(studentsNumber.value) !== null) 
 						{	
-							if(nameAndNumberValue[0].length >= 1)
-							{	
-								if(nameAndNumberValue[0].length === 6)
-								{
-									console.log("Data siswa tidak kosong")
+							console.log("Data siswa tidak kosong")
 
-									RandomCodeContainer(saveRandomCodeProperty)
-								}
-							}
+							RandomCodeContainer(randomNumberValue)
+
+							return  actionSubmit.preventDefault()
 						}
 							else
 							{	
 								console.log("Data siswa kosong")
-
-								return // PreventSubmit(formBody)
+								return disabledButton.disabled = false
 							}
 					}
 }
 // Baris akhir object
 
-// Function untuk menghentikan event
-function PreventSubmit(PreventForm)
-{
-	return PreventForm.preventDefault()
-}
-// Baris akhir function
-
 // Function untuk memunculkan angka acak
 function RandomCodeContainer(saveRandomCodeProperty)
 {
-	saveRandomCodeProperty = inputData.RandomNumberProperty
-
 	return saveRandomCodeProperty.style.display = "flex"
 }
 // Baris akhir function
 
 // Event submit untuk mengirim data
-const sendSubmitEvent = inputData.SubmitStudentsData
+const sendSubmitForm = inputData.FormContainer
 
-sendSubmitEvent.addEventListener("submit",Send_Student_Data,false)
-
-sendSubmitEvent.dispatchEvent(new Event("submit"))
+sendSubmitForm.addEventListener("submit",Send_Student_Data,false)
 // Baris akhir event
 
 // function untuk mengirim data para siswa
@@ -78,17 +64,22 @@ function Send_Student_Data()
 	// Variabel untuk menyimpan property dan method pada object
 	let submitDataFunction = inputData.CheckInputData()
 
-	let preventSubmitValue = inputData.FormContainer
+	const senderForm = inputData.FormContainer
 	// Baris akhir variabel
 
 	switch(submitDataFunction)
-	{ 
+	{ 	
+		case submitDataFunction :
 		case !null : return submitDataFunction
 			break
-		case null : return PreventSubmit(preventSubmitValue)
+		case null :
+		case !submitDataFunction : return senderForm.preventDefault()
 			break
 	}
+
+	return submitDataFunction
 }
+// Send_Student_Data()
 // Baris akhir function
 
 // Bagian object yang terdiri dari array dan element html
---
+++ Attendance_Succes/Redirect.js
@@ -0,0 +1,3 @@
+let textMessage = innerText("Hai")
+
+body.textMessage
